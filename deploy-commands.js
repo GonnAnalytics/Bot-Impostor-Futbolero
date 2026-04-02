08b19c16c86719e8fa3ec52f79e25af0f3ec5366
Gonzalo <gonzalo968825@email.com>
2026-04-02 03:21:31 -0300

    feat: registro global de comandos

--- deploy-commands.js
+++ deploy-commands.js
@@ -25,7 +25,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
   try {
     console.log('Registrando comandos...');
     await rest.put(
-      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands }
     );
     console.log('✅ Comandos registrados.');
